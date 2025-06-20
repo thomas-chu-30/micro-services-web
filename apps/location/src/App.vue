a81b7c582131f44dd3c1724c27bbbb25f523e8db
“thomas” <thomas.chu@thortron.com>
2025-06-20 21:11:47 +0800

    feat: add events service and theme components to location app, enhance calendar functionality and styles

--- apps/location/src/App.vue
+++ apps/location/src/App.vue
@@ -9,7 +9,11 @@ import {
 import '@schedule-x/theme-default/dist/index.css'
 import { ScheduleXCalendar } from '@schedule-x/vue'
 import MultiSelect from 'primevue/multiselect'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+
+import { createEventsServicePlugin } from '@schedule-x/events-service'
+
+const eventsServicePlugin = createEventsServicePlugin()
 
 //
 
@@ -25,43 +29,143 @@ const cities = ref([
 //
 // Do not use a ref here, as the calendar instance is not reactive, and doing so might cause issues
 // For updating events, use the events service plugin
-const calendarApp = createCalendar({
-  selectedDate: '2023-12-19',
-  locale: 'zh-CN',
-  isDark: false,
-  defaultView: 'month-grid',
-  views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
-  calendars: {
-    work: {
-      colorName: 'work',
-      lightColors: {
-        main: '#f91c45',
-        container: '#ffd2dc',
-        onContainer: '#59000d',
+const calendarApp = createCalendar(
+  {
+    selectedDate: '2023-12-19',
+    locale: 'zh-TW',
+    isDark: false,
+    defaultView: 'month-grid',
+    views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
+    calendars: {
+      work: {
+        colorName: 'work',
+        lightColors: {
+          main: '#f91c45',
+          container: '#ffd2dc',
+          onContainer: '#59000d',
+        },
+        darkColors: {
+          main: '#ffc0cc',
+          onContainer: '#ffdee6',
+          container: '#a24258',
+        },
       },
-      darkColors: {
-        main: '#ffc0cc',
-        onContainer: '#ffdee6',
-        container: '#a24258',
+    },
+
+    monthGridOptions: {
+      /**
+       * Number of events to display in a day cell before the "+ N events" button is shown
+       * */
+      nEventsPerDay: 3,
+    },
+    events: [
+      {
+        id: 1,
+        title: 'Event 1',
+        start: '2023-12-20 12:00',
+        end: '2023-12-20 13:00',
+        calendarId: 'work',
+        description: 'Event 1asdfasdad',
+        _customContent: {
+          monthAgenda: 'Event 1asdfasdad',
+        },
+      },
+      {
+        id: 1,
+        title: 'Event 1',
+        start: '2023-12-20 12:00',
+        end: '2023-12-20 13:00',
+        calendarId: 'work',
+        description: 'Event 1asdfasdad',
+        _customContent: {
+          monthAgenda: 'Event 1asdfasdad',
+        },
+      },
+      {
+        id: 1,
+        title: 'Event 1',
+        start: '2023-12-19',
+        end: '2023-12-19',
+        calendarId: 'work',
+        description: 'Event 1asdfasdad',
+        _customContent: {
+          monthAgenda: 'Event 1asdfasdad',
+        },
+      },
+      {
+        id: 1,
+        title: 'Event 1',
+        start: '2023-12-19',
+        end: '2023-12-19',
+        calendarId: 'work',
+        description: 'Event 1asdfasdad',
+        _customContent: {
+          monthAgenda: 'Event 1asdfasdad',
+        },
+      },
+      {
+        id: 1,
+        title: 'Event 1',
+        start: '2023-12-19',
+        end: '2023-12-19',
+        calendarId: 'work',
+        description: 'Event 1asdfasdad',
+        _customContent: {
+          monthAgenda: 'Event 1asdfasdad',
+        },
+      },
+      {
+        id: 1,
+        title: 'Event 1',
+        start: '2023-12-20 12:00',
+        end: '2023-12-20 13:00',
+        calendarId: 'work',
+        description: 'Event 1asdfasdad',
+        _customContent: {
+          monthAgenda: 'Event 1asdfasdad',
+        },
+      },
+      {
+        id: 1,
+        title: 'Event 1',
+        start: '2023-12-20 12:00',
+        end: '2023-12-20 13:00',
+        calendarId: 'work',
+        description: 'Event 1asdfasdad',
+        _customContent: {
+          monthAgenda: 'Event 1asdfasdad',
+        },
+      },
+
+      {
+        id: 2,
+        title: 'Event 2',
+        start: '2023-12-20 12:00',
+        end: '2023-12-20 13:00',
+      },
+    ],
+    callbacks: {
+      onEventClick(calendarEvent, event) {
+        console.log('onEventClick', calendarEvent, event)
       },
     },
   },
-  events: [
-    {
-      id: 1,
-      title: 'Event 1',
-      start: '2023-12-19',
-      end: '2023-12-19',
-      calendarId: 'work',
-    },
-    {
-      id: 2,
-      title: 'Event 2',
-      start: '2023-12-20 12:00',
-      end: '2023-12-20 13:00',
-    },
-  ],
+  [eventsServicePlugin],
+)
+
+onMounted(() => {
+  new Promise((resolve) => setTimeout(resolve, 1000))
+  calendarApp.eventsService.add({
+    title: 'Event 1  thomas test',
+    start: '2025-06-20 12:00',
+    end: '2025-06-20 13:00',
+    id: 1,
+  })
 })
+
+const id = (id: number) => {
+  console.log(id)
+}
 </script>
 
 <template>
@@ -76,7 +180,7 @@ const calendarApp = createCalendar({
       class="w-[200px]"
     />
 
-    <ScheduleXCalendar :calendar-app="calendarApp" class="flex-1" />
+    <ScheduleXCalendar :calendar-app="calendarApp" @id="id" class="flex-1" />
   </div>
 </template>
 
